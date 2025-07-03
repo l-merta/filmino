5c02024dc0a0373b093c209afef452a2577f26c3
l-merta <lukas.merta26@gmail.com>
2025-07-03 23:59:47 +0200

    Movie card setup

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,11 +1,24 @@
+import Image from "next/image";
+
 import { Skeleton } from "./ui/skeleton";
 
-export default function Card() {
-  return (
+import { MovieDetails } from "@/types/tmdbApi";
+
+interface CardProps {
+  details?: MovieDetails;
+}
+
+export default function Card({ details }: CardProps) {
+  if (!details) { return (
     <div className="flex flex-col gap-2">
       <Skeleton className="w-full aspect-[2/3]" />
       <Skeleton className="w-full h-5" />
       <Skeleton className="w-30 h-5" />
     </div>
-  );
+  )} else { return (
+    <div className="flex flex-col gap-2">
+      <Image src={'https://unsplash.it/1080/1920'} alt={details.title} width={undefined} height={undefined} className="w-full aspect-[2/3]" />
+      <span className="font-bold text-lg">{details.title}</span>
+    </div>
+  )}
 }
--- app/sections/List.tsx
+++ app/sections/List.tsx
@@ -1,16 +1,19 @@
 import Card from "@/components/Card";
+import { tmdb } from "@/hooks/useTmdb";
 
 interface ListProps {
   header: string;
 }
 
 export default function List({ header }: ListProps) {
+  const { data } = tmdb.movie.Popular();
+
   return (
     <div>
       <h2 className="font-bold text-2xl mb-4 opacity-80">{header}</h2>
       <div className="grid grid-cols-5 gap-y-10 gap-x-5">
         {Array.from({ length: 20 }).map((_, index) => (
-          <Card key={"card-" + index} />
+          <Card key={"card-" + index} details={data?.results[index]} />
         ))}
       </div>
     </div>
