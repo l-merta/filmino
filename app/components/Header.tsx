c7c19c44cd12f76d50e021899f312c0b0d404f7b
l-merta <lukas.merta26@gmail.com>
2025-07-09 23:23:35 +0200

    Header input and buttons

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,6 @@
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
 interface HeaderProps {
   active: 'filmy' | 'serialy';
 }
@@ -5,14 +8,18 @@ interface HeaderProps {
 export default function Header({ active }: HeaderProps) {
   return (
     <header className="flex items-center justify-between fixed top-0 !mx-auto">
-      <h1 className={(active == "filmy" ? "text-[var(--color-main)]" : "text-[var(--color-main-2)]") + " text-2xl font-bold"}>Filmino</h1>
-      <nav className="flex space-x-4">
+      <div className="flex items-center space-x-5">
+        <h1 className={(active == "filmy" ? "text-[var(--color-main)]" : "text-[var(--color-main-2)]") + " text-2xl font-bold"}>Filmino</h1>
+        <Input placeholder="Hledat" className="w-80"></Input>
+      </div>
+      <nav className="flex items-center space-x-4">
         <a href="/filmy" className={(active == "filmy" ? "text-[var(--color-main)]" : "opacity-80") + " font-semibold hover:opacity-70"}>
           Filmy
         </a>
         <a href="/serialy" className={(active == "serialy" ? "text-[var(--color-main-2)]" : "opacity-80") + " font-semibold hover:opacity-70"}>
           Seriály
         </a>
+        <Button variant="outline" className="ml-2">Přihlásit se</Button>
       </nav>
     </header>
   );
